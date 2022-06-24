b773ad3906de7d4394394074c2ca02b86e9c9f89
17seannnn <17seannnn@gmail.com>
2022-06-24 20:15:10 +0300

    feat: move

--- snake.cpp
+++ snake.cpp
@@ -1,17 +1,59 @@
+#include <curses.h>
+
+#include "curses.h"
+
 #include "snake.h"
 
 Snake::~Snake() {
+    Tail* temp;
 
+    while (first) {
+        temp = first;
+        first = first->next;
+        delete temp;
+    }
 }
 
 void Snake::Add(int x, int y) {
-    // If x and y != 1 then it's first element, else it's next part of tail
+    Tail* temp = new Tail;
+
+    temp->next = 0;
+    if (x == -1) {
+        last->next = temp;
+        last = temp;
+    } else {
+        temp->x = x;
+        temp->y = y;
+        first = temp;
+        last = temp;
+    }
 }
 
-bool Snake::SelfCollision() {
+bool Snake::SelfCollision() const {
     return false;
 }
 
 void Snake::Move() {
+    Hide();
+    first->x += dx;
+    if (first->x < 0)
+        first->x = curses.game_win_width-1;
+    else if (first->x >= curses.game_win_width)
+        first->x = 0;
+    first->y += dy;
+    if (first->y < 0)
+        first->y = curses.game_win_height-1;
+    else if (first->y >= curses.game_win_height)
+        first->y = 0;
+    Show();
+}
 
+void Snake::Show() const {
+    for (Tail* temp = first; temp; temp = temp->next)
+        mvwaddch(curses.game_win, temp->y, temp->x, '#');
+}
+
+void Snake::Hide() const {
+    for (Tail* temp = first; temp; temp = temp->next)
+        mvwaddch(curses.game_win, temp->y, temp->x, ' ');
 }
--- snake.h
+++ snake.h
@@ -2,21 +2,21 @@
 #define SNAKE_H
 
 class Snake {
-    struct tail {
+    struct Tail {
         int x, y;
-        struct tail* next;
+        struct Tail* next;
     };
 
     int dx, dy;
-    tail *first, *last;
+    Tail *first, *last;
 public:
     Snake(int first_x, int first_y) : dx(0), dy(0), first(0), last(0)
         { Add(first_x, first_y); }
     ~Snake();
 
-    void Add(int x = -1, int y = -1); // To add tail just snake.Add()
+    void Add(int x = -1, int y = -1); // To add tail just snake.Add(), TODO for first self add
     void SetDirection(int n_dx, int n_dy) { dx = n_dx; dy = n_dy; }
-    bool SelfCollision(); // True on self collision
+    bool SelfCollision() const; // True on self collision
     void Move();
 private:
     void Show() const;
